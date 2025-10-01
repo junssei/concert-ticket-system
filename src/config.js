fa7274404eb901b9602411d2ff8fbc2b02aa0768
juvncent <junevincentmagsayofernandez@gmail.com>
2025-10-02 02:23:00 +0800

    config changes

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const TICKETMASTER_API_KEY = "jKhPvHMAsgPdNaNUGQ1iNTjXLjfGtyQj";
 export const PAYPAL_CLIENT_ID = "AV-7EaCuzVrF72-sI3zJRPiAUsr8LbRgEOdVW7XZKBXf-6tcpkuhOnSM0VP4MdfbyDXPd6I4yXj8v2FL";
-export const API_BASE_URL = "shortline.proxy.rlwy.net:13640";
+export const API_BASE_URL = "concert-ticket-system-production.up.railway.app";
